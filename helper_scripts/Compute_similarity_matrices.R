7f9f162f37f90057b1043fe5128f3059e29bb2d5
RomFas <Romas-92@hotmail.com>
2022-09-04 17:54:40 -0500

    Update similarity matrices derivation and files

--- helper_scripts/Compute_similarity_matrices.R
+++ helper_scripts/Compute_similarity_matrices.R
@@ -1,61 +1,45 @@
 library(data.table)
 library(magrittr)
-#library(ade4)
-#library(ape)
-#library(dendextend)
-#library(ggplot2)
-#library(ggrepel)
 
-Template_alleles <- fread("Template_information.csv")[, MHC] %>% unique
+Template_alleles <- fread("Updated_template_information.csv")[, MHC] %>% unique
 
 freq_table <- fread("./mhcflurry.ba.frequency_matrices.csv")
 freq_table <- freq_table[cutoff_fraction == 0.001, !c("cutoff_fraction", "cutoff_count")]
 
-freq_table_8 <- freq_table[length == 8, !c("length")]
-freq_table_9 <- freq_table[length == 9, !c("length")]
-freq_table_10 <- freq_table[length == 10, !c("length")]
-freq_table_11 <- freq_table[length == 11, !c("length")]
-freq_table_12 <- freq_table[length == 12, !c("length")]
-freq_table_13 <- freq_table[length == 13, !c("length")]
-freq_table_14 <- freq_table[length == 14, !c("length")]
-freq_table_15 <- freq_table[length == 15, !c("length")]
+HELL <- function(c1, c2) {
+  mean(sqrt(rowSums((sqrt(c1) - sqrt(c2))^2)) / sqrt(2))
+}
 
 extract_matrix <- function(freq_table, allele_list, Template_alleles, nmer) {
-  euclidean <- function(a, b) sqrt(sum((a - b)^2))
   allele_list <- split(freq_table, by="allele", keep.by=FALSE)
+  allele_list <-lapply(allele_list, function(mat) mat[,-1])
   alleles_found_in_MHCFlurry <- names(allele_list)[names(allele_list) %in% Template_alleles]
   not_supported_alleles <- setdiff(Template_alleles, names(allele_list)[names(allele_list) %in% Template_alleles])
   filtered_allele_list <- allele_list[alleles_found_in_MHCFlurry]
   i <- 0
-  correlation_matrix <- lapply(allele_list, function(x) {
-    correlation <- sapply(filtered_allele_list, function(y) {
-      euclidean(x %>% as.matrix %>% t %>% as.vector, y %>% as.matrix %>% t %>% as.vector)
-    })
-    correlation <- c(correlation, rep(180, length(not_supported_alleles))) # Max possible euclidean distance 
+  similarity_matrix <- lapply(allele_list, function(x) {
+    similarity <- sapply(filtered_allele_list, function(y) {HELL(x, y)})
+    similarity <- c(similarity, rep(0, length(not_supported_alleles))) # Max possible euclidean distance 
     i <<- i + 1
     message(paste(i, "/", length(allele_list)),"\r",appendLF=FALSE)
     flush.console()
-    correlation
+    similarity
   })
-  correlation_matrix <- do.call(rbind, correlation_matrix)
-  colnames(correlation_matrix) <- c(names(filtered_allele_list), not_supported_alleles)
-  correlation_matrix <- rbind(correlation_matrix, matrix(180, nrow = length(not_supported_alleles), ncol = length(Template_alleles)))
-  rownames(correlation_matrix) <- c(names(allele_list), not_supported_alleles)
-  correlation_matrix[outer(rownames(correlation_matrix), colnames(correlation_matrix), "==")] <- 0
-  correlation_df <- as.data.table(correlation_matrix, keep.rownames = TRUE)
-  setnames(correlation_df, "rn", "Allele")
-  fwrite(correlation_df, paste0(nmer, "mer_similarity.csv"))
+  similarity_matrix <- do.call(rbind, similarity_matrix)
+  colnames(similarity_matrix) <- c(names(filtered_allele_list), not_supported_alleles)
+  similarity_matrix <- rbind(similarity_matrix, matrix(0, nrow = length(not_supported_alleles), ncol = length(Template_alleles)))
+  rownames(similarity_matrix) <- c(names(allele_list), not_supported_alleles)
+  similarity_matrix[outer(rownames(similarity_matrix), colnames(similarity_matrix), "==")] <- 0
+  similarity_df <- as.data.table(similarity_matrix, keep.rownames = TRUE)
+  setnames(similarity_df, "rn", "Allele")
+  fwrite(similarity_df, paste0(nmer, "mer_similarity.csv"))
 }
 
-extract_matrix(freq_table_8, allele_list, Template_alleles, "8")
-extract_matrix(freq_table_9, allele_list, Template_alleles, "9")
-extract_matrix(freq_table_10, allele_list, Template_alleles, "10")
-extract_matrix(freq_table_11, allele_list, Template_alleles, "11")
-extract_matrix(freq_table_12, allele_list, Template_alleles, "12")
-extract_matrix(freq_table_13, allele_list, Template_alleles, "13")
-extract_matrix(freq_table_14, allele_list, Template_alleles, "14")
-extract_matrix(freq_table_15, allele_list, Template_alleles, "15")
+for (nmer in seq(8,15)) {
+  extract_matrix(freq_table[length == nmer, !c("length")], allele_list, Template_alleles, nmer)
+}
 
+# What I want to do here is calculate 
 
 # Let's see if I can make a tree:
 #correlation_matrix[alleles_found_in_MHCFlurry, alleles_found_in_MHCFlurry]
