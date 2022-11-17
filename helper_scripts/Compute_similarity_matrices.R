f8be16fb263e73f073fd3c08c0aa73c48bebc898
RomFas <Romas-92@hotmail.com>
2022-11-17 17:08:49 -0600

    Update similarity matrix computation script

--- helper_scripts/Compute_similarity_matrices.R
+++ helper_scripts/Compute_similarity_matrices.R
@@ -1,13 +1,13 @@
 library(data.table)
 library(magrittr)
 
-Template_alleles <- fread("Updated_template_information.csv")[, MHC] %>% unique
+Template_alleles <- fread("Template_DB_information.csv")[, MHC] %>% unique
 
 freq_table <- fread("./mhcflurry.ba.frequency_matrices.csv")
 freq_table <- freq_table[cutoff_fraction == 0.001, !c("cutoff_fraction", "cutoff_count")]
 
 HELL <- function(c1, c2) {
-  mean(sqrt(rowSums((sqrt(c1) - sqrt(c2))^2)) / sqrt(2))
+  1 - mean(sqrt(rowSums((sqrt(c1) - sqrt(c2))^2)) / sqrt(2))
 }
 
 extract_matrix <- function(freq_table, allele_list, Template_alleles, nmer) {
@@ -19,7 +19,7 @@ extract_matrix <- function(freq_table, allele_list, Template_alleles, nmer) {
   i <- 0
   similarity_matrix <- lapply(allele_list, function(x) {
     similarity <- sapply(filtered_allele_list, function(y) {HELL(x, y)})
-    similarity <- c(similarity, rep(0, length(not_supported_alleles))) # Max possible euclidean distance 
+    similarity <- c(similarity, rep(0, length(not_supported_alleles))) # Min possible Hellinger similarity 
     i <<- i + 1
     message(paste(i, "/", length(allele_list)),"\r",appendLF=FALSE)
     flush.console()
@@ -29,7 +29,8 @@ extract_matrix <- function(freq_table, allele_list, Template_alleles, nmer) {
   colnames(similarity_matrix) <- c(names(filtered_allele_list), not_supported_alleles)
   similarity_matrix <- rbind(similarity_matrix, matrix(0, nrow = length(not_supported_alleles), ncol = length(Template_alleles)))
   rownames(similarity_matrix) <- c(names(allele_list), not_supported_alleles)
-  similarity_matrix[outer(rownames(similarity_matrix), colnames(similarity_matrix), "==")] <- 0
+  similarity_matrix[substr(rownames(similarity_matrix), 1, 4) == "Gaga", substr(colnames(similarity_matrix), 1, 4) == "Gaga"] <- 0.5
+  similarity_matrix[outer(rownames(similarity_matrix), colnames(similarity_matrix), "==")] <- 1 # Max possible Hellinger similarity 
   similarity_df <- as.data.table(similarity_matrix, keep.rownames = TRUE)
   setnames(similarity_df, "rn", "Allele")
   fwrite(similarity_df, paste0(nmer, "mer_similarity.csv"))
@@ -38,6 +39,8 @@ extract_matrix <- function(freq_table, allele_list, Template_alleles, nmer) {
 for (nmer in seq(8,15)) {
   extract_matrix(freq_table[length == nmer, !c("length")], allele_list, Template_alleles, nmer)
 }
+
+#### CAUTION!!! Go and increase similarity between chciken alleles manually after this!
 
 # What I want to do here is calculate 
 
